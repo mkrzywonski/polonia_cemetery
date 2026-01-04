65eed4a237c2c92bbfb925a6788af45cec97a2c8
Mike Krzywonski <mike@krzywonski.me>
2026-01-04 08:06:17 -0600

    Add Polonia site

---
+++ assets/app.js
@@ -0,0 +1,703 @@
+const records = [
+  {
+    "name": "Polonia Cemetery Entrance",
+    "image": "027.jpg"
+  },
+  {
+    "name": "Historical Marker",
+    "image": "035.jpg"
+  },
+  {
+    "name": "Old Church Site",
+    "image": "014.jpg"
+  },
+  {
+    "name": "Old Well",
+    "image": "017.jpg"
+  },
+  {
+    "name": "Balandran, Saturnino",
+    "image": "089.jpg"
+  },
+  {
+    "name": "Bienek, Edmund",
+    "image": null
+  },
+  {
+    "name": "Beran, Nettie Poldrack",
+    "image": "129.jpg"
+  },
+  {
+    "name": "Beran, Tony Sylvester",
+    "image": "129.jpg"
+  },
+  {
+    "name": "Bienek, David Eugene Sr.",
+    "image": "138.jpg"
+  },
+  {
+    "name": "Bienek, JoAnn Elizabeth (Renz)",
+    "image": "138.jpg"
+  },
+  {
+    "name": "Bienek, Frances C.",
+    "image": "119.jpg"
+  },
+  {
+    "name": "Bienek, Gladys",
+    "image": "031.jpg"
+  },
+  {
+    "name": "Bienek, Josephine Frances",
+    "image": "057.jpg"
+  },
+  {
+    "name": "Bienek, Vincenta A.",
+    "image": "119.jpg"
+  },
+  {
+    "name": "Bieniek, Zofia",
+    "image": "125.jpg"
+  },
+  {
+    "name": "Bonkowski, Genevieve Agnes",
+    "image": "033.jpg"
+  },
+  {
+    "name": "Bonkowski, Eleanora",
+    "image": "025.jpg"
+  },
+  {
+    "name": "Bonkowski, John J.",
+    "image": "049.jpg"
+  },
+  {
+    "name": "Bonkowski, Martin",
+    "image": "069.jpg"
+  },
+  {
+    "name": "Bonkowski, Rosa A.",
+    "image": "049.jpg"
+  },
+  {
+    "name": "Bonkowski, Theodora",
+    "image": "107.jpg"
+  },
+  {
+    "name": "Bostelman, DeForest G.",
+    "image": "020.jpg"
+  },
+  {
+    "name": "Bostelman, Izabel T.",
+    "image": "039.jpg"
+  },
+  {
+    "name": "Colby, David George",
+    "image": "019.jpg"
+  },
+  {
+    "name": "Colby, Rose Lempa",
+    "image": "019.jpg"
+  },
+  {
+    "name": "Dedak, Anna",
+    "image": "116.jpg"
+  },
+  {
+    "name": "Dedek, Kate",
+    "image": "060.jpg"
+  },
+  {
+    "name": "Dzenowski, Chester Steve",
+    "image": "012.jpg"
+  },
+  {
+    "name": "Dzenowski, Felix",
+    "image": "030.jpg"
+  },
+  {
+    "name": "Dzenowski, Lois Flores",
+    "image": "012.jpg"
+  },
+  {
+    "name": "Dzenowski, Veronica",
+    "image": "117.jpg"
+  },
+  {
+    "name": "Dzienowski, John Henry",
+    "image": "046.jpg"
+  },
+  {
+    "name": "Dzienowski, Martha Wanda",
+    "image": "046.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Annie",
+    "image": "100.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Dorothy",
+    "image": "021.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Edward",
+    "image": "024.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Joseph",
+    "image": "056.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Louis",
+    "image": "066.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Lucia",
+    "image": "067.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Mary",
+    "image": "070.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Paul",
+    "image": "078.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Stella",
+    "image": "098.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Simon",
+    "image": "091.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Steve",
+    "image": "100.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Tecla",
+    "image": "106.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Victoria",
+    "image": "118.jpg"
+  },
+  {
+    "name": "Dzierzanowski, Walter",
+    "image": "121.jpg"
+  },
+  {
+    "name": "Everett, John Richard",
+    "image": "130.jpg"
+  },
+  {
+    "name": "Ewald, Robert Lee",
+    "image": "088.jpg"
+  },
+  {
+    "name": "Foryszewski, Felix",
+    "image": "029.jpg"
+  },
+  {
+    "name": "Foryszewski, Frances",
+    "image": "029.jpg"
+  },
+  {
+    "name": "Foryszewski, Joe",
+    "image": "042.jpg"
+  },
+  {
+    "name": "Foryszewski, John",
+    "image": "052.jpg"
+  },
+  {
+    "name": "Foryszpwski, Ludwika",
+    "image": "068.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Anna",
+    "image": "005.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Connie T.",
+    "image": "015.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Irene B.",
+    "image": "099.jpg"
+  },
+  {
+    "name": "Grabarkewitz, John",
+    "image": "005.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Leopold",
+    "image": "064.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Regina",
+    "image": "085.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Steve J.",
+    "image": "099.jpg"
+  },
+  {
+    "name": "Grabarkewitz, Theodore R.",
+    "image": "015.jpg"
+  },
+  {
+    "name": "Harris, Mildred Lempa",
+    "image": "075.jpg"
+  },
+  {
+    "name": "Harris, Richard Milton",
+    "image": "086.jpg"
+  },
+  {
+    "name": "Holmes, Josephine S.",
+    "image": "058.jpg"
+  },
+  {
+    "name": "Kalinowski, Baby Girl",
+    "image": "007.jpg"
+  },
+  {
+    "name": "Kalinowski, Bill",
+    "image": "008.jpg"
+  },
+  {
+    "name": "Kalinowski, Cecelia T.",
+    "image": "124.jpg"
+  },
+  {
+    "name": "Kalinowski, Marie A. Schwab",
+    "image": "135.jpg"
+  },
+  {
+    "name": "Kalinowski, Mary",
+    "image": "071.jpg"
+  },
+  {
+    "name": "Kalinowski, Mike T.",
+    "image": "135.jpg"
+  },
+  {
+    "name": "Kalinowski, Simon",
+    "image": "092.jpg"
+  },
+  {
+    "name": "Kalinowski, Zigmond S.",
+    "image": "124.jpg"
+  },
+  {
+    "name": "Karstetter, John R.",
+    "image": "045.jpg"
+  },
+  {
+    "name": "Karstetter, Mabel S.",
+    "image": "045.jpg"
+  },
+  {
+    "name": "Kenney, Leona Charlotte",
+    "image": "131.jpg"
+  },
+  {
+    "name": "Kenney, William Harry",
+    "image": "131.jpg"
+  },
+  {
+    "name": "Krzywonski, Elliott Richard",
+    "image": "127.jpg"
+  },
+  {
+    "name": "Krzywonski, Frank C.",
+    "image": "032.jpg"
+  },
+  {
+    "name": "Krzywonski, Josephine M. Lempa",
+    "image": "109.jpg"
+  },
+  {
+    "name": "Krzywonski, Mike J.",
+    "image": "074.jpg"
+  },
+  {
+    "name": "Krzywonski, Theodore",
+    "image": "108.jpg"
+  },
+  {
+    "name": "Krzywonski, Tom",
+    "image": "109.jpg"
+  },
+  {
+    "name": "Krzywosinski, John (1847-1919)",
+    "image": "048.jpg"
+  },
+  {
+    "name": "Krzywosinski, John (1901-1968)",
+    "image": "050.jpg"
+  },
+  {
+    "name": "Krzywosinski, Mary",
+    "image": "072.jpg"
+  },
+  {
+    "name": "Krzywosinski, Prakseda",
+    "image": "048.jpg"
+  },
+  {
+    "name": "Krzywosinski, Stephanie",
+    "image": "050.jpg"
+  },
+  {
+    "name": "Lempa, Adela",
+    "image": "002.jpg"
+  },
+  {
+    "name": "Lempa, Joe",
+    "image": "043.jpg"
+  },
+  {
+    "name": "Lempar, Conrad Joseph",
+    "image": "126.jpg"
+  },
+  {
+    "name": "Lempar, Emilie Marie Dzienowski",
+    "image": "026.jpg"
+  },
+  {
+    "name": "Little, Derril Beryl",
+    "image": "137.jpg"
+  },
+  {
+    "name": "Little, Veronica Rose Krzywosinski",
+    "image": "137.jpg"
+  },
+  {
+    "name": "Mohermann, Jean",
+    "image": "040.jpg"
+  },
+  {
+    "name": "Nacio, Josdej H.",
+    "image": null
+  },
+  {
+    "name": "Perry, Edward Keith",
+    "image": "134.jpg"
+  },
+  {
+    "name": "Perry, Heather Lynnette",
+    "image": "133.jpg"
+  },
+  {
+    "name": "Perry, Rose Marie",
+    "image": "134.jpg"
+  },
+  {
+    "name": "Pieniazek, Michael",
+    "image": "073.jpg"
+  },
+  {
+    "name": "Radowitz, Ike L.",
+    "image": "038.jpg"
+  },
+  {
+    "name": "Rakowitz, Angela",
+    "image": null
+  },
+  {
+    "name": "Rakowitz, Annie V.",
+    "image": "084.jpg"
+  },
+  {
+    "name": "Rakowitz, Janie",
+    "image": "044.jpg"
+  },
+  {
+    "name": "Rakowitz, John",
+    "image": "044.jpg"
+  },
+  {
+    "name": "Rakowitz, Pete",
+    "image": "080.jpg"
+  },
+  {
+    "name": "Rakowitz, Sylvester J.",
+    "image": "084.jpg"
+  },
+  {
+    "name": "Reese, Claudette Jo Hejl",
+    "image": "128.jpg"
+  },
+  {
+    "name": "Reese, Timothy William",
+    "image": "128.jpg"
+  },
+  {
+    "name": "Scholwinski, Age 9",
+    "image": "090.jpg"
+  },
+  {
+    "name": "Scholwinski, Frank",
+    "image": "031.jpg"
+  },
+  {
+    "name": "Scholwinski, Mary",
+    "image": "031.jpg"
+  },
+  {
+    "name": "Scholwinski, Sophie",
+    "image": "094.jpg"
+  },
+  {
+    "name": "Sykowski, Joseph",
+    "image": "061.jpg"
+  },
+  {
+    "name": "Sykowski, Katherine",
+    "image": "061.jpg"
+  },
+  {
+    "name": "Urbanski, Angela",
+    "image": "004.jpg"
+  },
+  {
+    "name": "Urbanski, George E.",
+    "image": null
+  },
+  {
+    "name": "Urbanski, Helen",
+    "image": null
+  },
+  {
+    "name": "Urbanski, Ignatz",
+    "image": "037.jpg"
+  },
+  {
+    "name": "Urbanski, John",
+    "image": "053.jpg"
+  },
+  {
+    "name": "Urbanski, Josephine",
+    "image": "037.jpg"
+  },
+  {
+    "name": "Urbanski, Mary",
+    "image": "101.jpg"
+  },
+  {
+    "name": "Urbanski, Steve",
+    "image": "101.jpg"
+  },
+  {
+    "name": "Viswiesk, John",
+    "image": "054.jpg"
+  },
+  {
+    "name": "Walenta, Adolph",
+    "image": "003.jpg"
+  },
+  {
+    "name": "Walenta, Billy Ray",
+    "image": "132.jpg"
+  },
+  {
+    "name": "Walenta, Bonnie",
+    "image": "003.jpg"
+  },
+  {
+    "name": "Walenta, Carla S.",
+    "image": "011.jpg"
+  },
+  {
+    "name": "Walenta, Joyce Marie",
+    "image": "132.jpg"
+  },
+  {
+    "name": "Walenta, Robert J.",
+    "image": "087.jpg"
+  },
+  {
+    "name": "Wilhelm, Perry James",
+    "image": "093.jpg"
+  },
+  {
+    "name": "Wilhelm, Sophie Rosie",
+    "image": "093.jpg"
+  },
+  {
+    "name": "Wisnewski, France",
+    "image": null
+  },
+  {
+    "name": "Wisnieski, Pearl Agnes",
+    "image": "079.jpg"
+  },
+  {
+    "name": "Wisniewski, Steve",
+    "image": "104.jpg"
+  },
+  {
+    "name": "Zaleski, Antola",
+    "image": "006.jpg"
+  },
+  {
+    "name": "Zaleski, Constance",
+    "image": "016.jpg"
+  },
+  {
+    "name": "Zaleski, George E.",
+    "image": "034.jpg"
+  },
+  {
+    "name": "Zaleski, Jimmie Gerald",
+    "image": "041.jpg"
+  },
+  {
+    "name": "Zaleski, John B.",
+    "image": "051.jpg"
+  },
+  {
+    "name": "Zaleski, Josephine",
+    "image": "059.jpg"
+  },
+  {
+    "name": "Zaleski, Kunegunda",
+    "image": null
+  },
+  {
+    "name": "Zaleski, Maggie M.",
+    "image": "016.jpg"
+  },
+  {
+    "name": "Zaleski, Nellie",
+    "image": "136.jpg"
+  },
+  {
+    "name": "Zaleski, Wyadyslaw",
+    "image": "123.jpg"
+  },
+  {
+    "name": "Zaleski, Wyadysyawa",
+    "image": "123.jpg"
+  },
+  {
+    "name": "Zalewski, Anna",
+    "image": "120.jpg"
+  },
+  {
+    "name": "Zalewski, Wailenti",
+    "image": "120.jpg"
+  },
+  {
+    "name": "Zaloski, John Jr.",
+    "image": "055.jpg"
+  },
+  {
+    "name": "Zolewski, Raimut",
+    "image": "082.jpg"
+  }
+];
+
+const listEl = document.querySelector('[data-records]');
+const countEl = document.querySelector('[data-count]');
+const searchEl = document.querySelector('[data-search]');
+const photoOnlyEl = document.querySelector('[data-photo-only]');
+const totalCountEl = document.querySelector('[data-total-count]');
+const photoCountEl = document.querySelector('[data-photo-count]');
+const modalEl = document.querySelector('[data-modal]');
+const modalImg = modalEl.querySelector('img');
+const modalName = modalEl.querySelector('[data-modal-name]');
+const modalClose = modalEl.querySelector('[data-modal-close]');
+
+const revealEls = document.querySelectorAll('[data-reveal]');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+revealEls.forEach((el, index) => {
+  el.style.setProperty('--delay', `${Math.min(index * 90, 600)}ms`);
+  observer.observe(el);
+});
+
+const normalize = (value) => value.toLowerCase().replace(/\s+/g, ' ').trim();
+
+const totalCount = records.length;
+const photoCount = records.filter((record) => record.image).length;
+
+if (totalCountEl) totalCountEl.textContent = totalCount;
+if (photoCountEl) photoCountEl.textContent = photoCount;
+
+const buildCard = (record) => {
+  const card = document.createElement('button');
+  card.className = record.image ? 'record-card has-photo' : 'record-card';
+  card.type = 'button';
+  card.innerHTML = `
+    <span class="record-name">${record.name}</span>
+    <span class="record-meta">${record.image ? 'Photo available' : 'No photo yet'}</span>
+  `;
+  if (!record.image) {
+    card.disabled = true;
+  } else {
+    card.addEventListener('click', () => {
+      modalImg.src = `images/${record.image}`;
+      modalImg.alt = `Headstone photo for ${record.name}`;
+      modalName.textContent = record.name;
+      modalEl.classList.add('is-open');
+      modalEl.setAttribute('aria-hidden', 'false');
+    });
+  }
+  return card;
+};
+
+const render = () => {
+  const query = normalize(searchEl.value);
+  const photoOnly = photoOnlyEl.checked;
+
+  const filtered = records.filter((record) => {
+    if (photoOnly && !record.image) return false;
+    if (!query) return true;
+    return normalize(record.name).includes(query);
+  });
+
+  listEl.innerHTML = '';
+  filtered.forEach((record) => {
+    listEl.appendChild(buildCard(record));
+  });
+
+  countEl.textContent = `${filtered.length} of ${records.length} records`;
+};
+
+const closeModal = () => {
+  modalEl.classList.remove('is-open');
+  modalEl.setAttribute('aria-hidden', 'true');
+};
+
+modalClose.addEventListener('click', closeModal);
+modalEl.addEventListener('click', (event) => {
+  if (event.target === modalEl) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
+
+searchEl.addEventListener('input', render);
+photoOnlyEl.addEventListener('change', render);
+
+render();
